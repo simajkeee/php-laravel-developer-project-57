0f6e752f8d8f4d365f986ba39783ade0d19aa9cd
Alex Karelin <alex.karelin@yournextagency.com>
2024-05-01 07:44:14 +0300

    add test for TaskStatusControllerTest

--- tests/Feature/TaskStatusControllerTest.php
+++ tests/Feature/TaskStatusControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\TaskStatus;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -15,12 +16,46 @@ class TaskStatusControllerTest extends TestCase
     public function test_index_method_displays_task_statuses(): void
     {
         $taskStatuses = TaskStatus::factory()->count(3)->create();
-        $route = route('task-statuses.index');
+        $route = route('task_statuses.index');
         $response = $this->get($route);
 
         $response->assertOk();
         foreach ($taskStatuses as $taskStatus) {
             $response->assertSeeText($taskStatus->name);
         }
+    }
+
+    public function test_index_method_doesnt_show_status_after_deletion(): void
+    {
+        $user = User::factory()->create();
+        $taskStatuses = TaskStatus::factory()->count(3)->create();
+        $toDelete = $taskStatuses->first();
+
+        $routeDestroy = route('task_statuses.destroy', ['task_status' => $toDelete->id]);
+        $responseDestroy = $this->actingAs($user)->delete($routeDestroy);
+        $responseDestroy->assertRedirect();
+
+        $routeIndex = route('task_statuses.index');
+        $responseIndex = $this->get($routeIndex);
+        $responseIndex->assertOk();
+        $responseIndex->assertDontSeeText($toDelete->name);
+    }
+
+    public function test_index_method_shows_updated_status(): void
+    {
+        $user = User::factory()->create();
+        $taskStatuses = TaskStatus::factory()->count(3)->create();
+        $toUpdate = $taskStatuses->first();
+
+        $updateStatus = 'Test Update Status';
+        $routeUpdate = route('task_statuses.update', ['task_status' => $toUpdate->id]);
+        $responseUpdate = $this->actingAs($user)->patch($routeUpdate, ['name' => $updateStatus]);
+        $responseUpdate->assertRedirect();
+
+        $routeIndex = route('task_statuses.index');
+        $responseIndex = $this->get($routeIndex);
+        $responseIndex->assertOk();
+        $responseIndex->assertSeeText($updateStatus);
+
     }
 }
